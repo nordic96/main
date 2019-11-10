4fe44bfbf2a54bf0f7cc99cdc1c18e06de43faa5
Gi Hun Ko <rhrlgns96@gmail.com>
2019-11-10 12:39:41 +0800

    fixes unit test for Messages.

--- src/main/java/com/typee/commons/core/Messages.java
+++ src/main/java/com/typee/commons/core/Messages.java
@@ -4,7 +4,6 @@ package com.typee.commons.core;
  * Container for user visible messages.
  */
 public class Messages {
-
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_ENGAGEMENT_DISPLAYED_INDEX = "The engagement index provided is invalid";
--- src/test/java/com/typee/commons/core/MessagesTest.java
+++ src/test/java/com/typee/commons/core/MessagesTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 class MessagesTest {
     @Test
     void messageValidTest() {
+        Messages messages = new Messages();
         assertEquals(Messages.MESSAGE_ENGAGEMENT_LISTED_OVERVIEW, "%1$d engagements listed!");
         assertEquals(Messages.MESSAGE_UNKNOWN_COMMAND, "Unknown command");
         assertEquals(Messages.MESSAGE_INVALID_COMMAND_FORMAT, "Invalid command format! \n%1$s");
