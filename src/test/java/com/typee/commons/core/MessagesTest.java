03961fca168ac51a1c8aeb919766ee4423e75211
Gi Hun Ko <rhrlgns96@gmail.com>
2019-11-10 12:35:40 +0800

    adds unit test for Messages class.

---
+++ src/test/java/com/typee/commons/core/MessagesTest.java
@@ -0,0 +1,15 @@
+package com.typee.commons.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MessagesTest {
+    @Test
+    void messageValidTest() {
+        assertEquals(Messages.MESSAGE_ENGAGEMENT_LISTED_OVERVIEW, "%1$d engagements listed!");
+        assertEquals(Messages.MESSAGE_UNKNOWN_COMMAND, "Unknown command");
+        assertEquals(Messages.MESSAGE_INVALID_COMMAND_FORMAT, "Invalid command format! \n%1$s");
+        assertEquals(Messages.MESSAGE_INVALID_ENGAGEMENT_DISPLAYED_INDEX, "The engagement index provided is invalid");
+    }
+}
